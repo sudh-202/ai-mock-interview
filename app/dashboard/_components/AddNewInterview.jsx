a0234e3fa2dfc8108d7b715c32f69fa816616047
DigitallyNext <digitallynext2017@gmail.com>
2024-09-28 15:13:31 +0530

    the changes

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -7,7 +7,7 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
+//   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,6 @@
-import { UserButton } from "@clerk/nextjs";
+// import { UserButton } from "@clerk/nextjs";
 import React from "react";
-import Header from "./_components/Header";
+// import Header from "./_components/Header";
 import AddNewInterview from "./_components/AddNewInterview";
 
 function Dashboard() {
